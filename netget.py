84d69a6c77ed14f470c40d38d4b08724e4b94966
Qilei Jiang <qileij@nvidia.com>
2020-10-12 16:47:52 +0800

    retry on timeout error. make it a package.

--- netget.py
+++ netget.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import json
+import socket
 import urllib.request
 from distutils.dir_util import copy_tree
 import zipfile
@@ -8,6 +10,22 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def retry_on_timeout(fn):
+    def fn_with_retry(*args, **kwargs):
+        max_retry = 5
+        for __ in range(max_retry-1):
+            try:
+                return fn(*args, **kwargs)
+            except urllib.error.URLError as err:
+                if isinstance(err.reason, TimeoutError):
+                    logger.debug('TimeoutError occured, retrying...')
+                    continue
+                else:
+                    raise
+        return fn(*args, **kwargs)
+    return fn_with_retry
+
+@retry_on_timeout
 def get_zip_and_extract(url, save_to, skip_first_folder=True):
     save_to = os.path.abspath(save_to)
     zip_name = 'temp.zip'
@@ -22,7 +40,7 @@ def get_zip_and_extract(url, save_to, skip_first_folder=True):
             with zipfile.ZipFile(temp_zip_path) as zf:
                 if skip_first_folder:
                     member_name = zf.namelist()[0].strip('/')
-                    logger.debug('extracting only %s to %s' 
+                    logger.debug('extracting only %s to %s...' 
                         % (member_name, save_to))
                     zf.extractall(path=temp_dir)
                     member_path = os.path.join(temp_dir, member_name)
@@ -30,6 +48,12 @@ def get_zip_and_extract(url, save_to, skip_first_folder=True):
                 else:
                     logger.debug('extracting to %s...' % save_to)
                     zf.extractall(path=save_to)
+
+@retry_on_timeout
+def get_json(url):
+    with urllib.request.urlopen(url) as s:
+        html = s.read()
+        return json.loads(html)
 
 if __name__ == '__main__':
     logging.basicConfig(
